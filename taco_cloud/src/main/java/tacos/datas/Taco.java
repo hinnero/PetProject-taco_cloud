3f62bd185de5934de2703820bc66b199f17d3d56
hinnero <93425258+hinnero@users.noreply.github.com>
2022-11-07 11:00:20 +0300

    second commit

---
+++ taco_cloud/src/main/java/tacos/datas/Taco.java
@@ -0,0 +1,29 @@
+package tacos.datas;
+
+import lombok.Data;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+@Data
+public class Taco {
+
+    private Long id;
+
+    private Date createdAt = new Date();
+
+    @NotNull
+    @Size(min = 3, message = "Name must be at least 3 characters long")
+    private String name;
+
+    @NotNull
+    @Size(min = 1, message = "You must choose at least 1 ingredient")
+    private List<Ingredient> ingredients = new ArrayList<>();
+
+    public void addIngredient(Ingredient ingredient) {
+        this.ingredients.add(ingredient);
+    }
+}
